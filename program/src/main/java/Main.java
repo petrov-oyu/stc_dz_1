4ab5e0ba286719c33a6b279634a5ea03cd95d388
Petrov_OYu <petrov-oyu@nefteavtomatika.ru>
2020-04-24 19:31:50 +0500

    simulate error NoSuchMethodException

--- program/src/main/java/Main.java
+++ program/src/main/java/Main.java
@@ -1,9 +1,6 @@
 public class Main {
-	static int[] helloWorld = {1, 2, 3, 4, 5};
 
-	public static void main(String[] args) {
-		for (int i = 0; i < 6; i++) {
-			System.out.println(helloWorld[i]);
-		}
+	public static void main(String[] args) throws NoSuchMethodException {
+		throw new NoSuchMethodException("cannot be invoke");
 	}
 }
