4ffe26997e5a36cf3d2f231a4c5de94f71dc6840
Petrov_OYu <petrov-oyu@nefteavtomatika.ru>
2020-04-24 20:13:23 +0500

    homework-2-2-1

--- program/src/main/java/Main.java
+++ program/src/main/java/Main.java
@@ -1,6 +1,35 @@
-public class Main {
+import java.util.Random;
 
-	public static void main(String[] args) throws NoSuchMethodException {
-		throw new NoSuchMethodException("cannot be invoke");
+/**
+ * Main class.
+ * Start point for all operations
+ *
+ * @author Petrov-OYu
+ */
+public class Main {
+	private static int arrayQuantity = 100000000;
+	private static double maxDoubleValue = 10.0;
+	private static double minDoubleValue = -0.1;
+
+	/**
+	 * Start point for all operations
+	 */
+	public static void main(String[] args) throws Exception {
+		for (int i = 0; i < arrayQuantity; i++) {
+			double random = minDoubleValue + (new Random().nextDouble() * (maxDoubleValue - minDoubleValue));
+			specificActionWithSQRT(random);
+		}
+	}
+
+	private static void specificActionWithSQRT(double value) throws Exception {
+		Double sqrtValue = Math.sqrt(value);
+
+		if (sqrtValue.isNaN()) {
+			throw new Exception("value " + value + " less than zero");
+		}
+
+		if (Math.round(sqrtValue * sqrtValue) == value) {
+			System.out.println(value);
+		}
 	}
 }
